07a64b43b584537ce514d908b493be98be9dbab7
Mohit Vishwakarma <mohitvishwa813@gmail.com>
2025-08-14 18:50:25 +0530

    fixed issue

--- Backend/Routes/Counts.js
+++ Backend/Routes/Counts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../Models/User");
 const Organizer = require("../models/EventOrganizer");
 const AddService = require("../models/AddService");
 
